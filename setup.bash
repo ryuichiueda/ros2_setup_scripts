4a068c48ee605530ffcc2ed2cb91c85aa2c90141
Ryuichi Ueda <ryuichiueda@gmail.com>
2020-11-27 00:34:09 +0000

    Remove key process

--- setup.bash
+++ setup.bash
@@ -1,14 +1,19 @@
 #!/bin/bash -e 
 
 sudo apt-get install curl gnupg2
-curl http://repo.ros2.org/repos.key |
-sudo apt-key add - 
+#curl http://repo.ros2.org/repos.key |
+#sudo apt-key add - 
 
-echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu bionic main" | sudo tee /etc/apt/sources.list.d/ros2-latest.list
+UBUNTU_VER=$(lsb_release -sc)
+ROS_VER=dashing
+[ "$UBUNTU_VER" = "focal" ] && ROS_VER=foxy
+
+echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu ${UBUNTU_VER} main" |
+sudo tee /etc/apt/sources.list.d/ros2-latest.list
 
 sudo apt-get update
 
-sudo apt-get install ros-dashing-desktop python3-colcon-common-extensions python3-rosdep python3-argcomplete
+sudo apt-get install ros-${ROS_VER}-desktop python3-colcon-common-extensions python3-rosdep python3-argcomplete
 
 sudo rm -f /etc/ros/rosdep/sources.list.d/20-default.list
 sudo rosdep init
